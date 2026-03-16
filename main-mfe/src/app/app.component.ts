8d2e374708911fdbdcefca6baaa0a7fddcaf51bc
dev-andrei-v <hello@andreidev.ro>
2026-03-16 22:32:03 +0200

    migrate to Clarity

--- main-mfe/src/app/app.component.ts
+++ main-mfe/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
 import { WidgetComponent } from './widget/widget.component';
 import { RemoteWidgetComponent } from './remote-widget/remote-widget.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, WidgetComponent, RemoteWidgetComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, ClarityModule, WidgetComponent, RemoteWidgetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- pcs-mfe/src/bootstrap.ts
+++ pcs-mfe/src/bootstrap.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import '@cds/core/icon/register.js';
+import { ClarityIcons, displayIcon } from '@cds/core/icon';
+
+ClarityIcons.addIcons(displayIcon);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
