e6ef64980e77849e6e45e4519f5b4c4170cbfe9e
dev-andrei-v <hello@andreidev.ro>
2026-03-16 22:00:10 +0200

    Implement remote MFE widgets sample

--- main-mfe/src/app/app.component.ts
+++ main-mfe/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { WidgetComponent } from './widget/widget.component';
+import { RemoteWidgetComponent } from './remote-widget/remote-widget.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, WidgetComponent, RemoteWidgetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
